e2bc6ce87ac83d1ceb950e81ff15a0dd991a4bc2
Miniseero <cmartinezp@usm.cl>
2023-10-21 02:18:47 -0300

    Diseño de preguntas hecho.

--- TreeCare/urls.py
+++ TreeCare/urls.py
@@ -17,8 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from Web import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index),
-    
+    path('quiz.html',views.quiz),
 ]
